bc47e2dc89c33de7cc7f21e79da7877e09b91959
“ManonCacciatore” <cacciatoremanon8@gmail.com>
2025-10-05 13:40:08 +0200

    Fonction findAllWithPostCount()

--- app/models/categoriesModel.php
+++ app/models/categoriesModel.php
@@ -13,3 +13,16 @@ function findAll(PDO $connexion)
     $rs = $connexion->query($sql);
     return $rs->fetchAll(PDO::FETCH_ASSOC);
 }
+
+
+function findAllWithPostCount(PDO $connexion)
+{
+    $sql = "SELECT c.*, COUNT(p.id) AS post_count
+        FROM categories c
+        LEFT JOIN posts p ON p.category_id = c.id
+        GROUP BY c.id
+        ORDER BY c.name ASC;
+    ";
+    $rs = $connexion->query($sql);
+    return $rs->fetchAll(PDO::FETCH_ASSOC);
+}
--- app/views/categories/_index.php
+++ app/views/categories/_index.php
@@ -1,11 +1,11 @@
 <ul class="menu-link">
     <?php
     include_once '../app/models/categoriesModel.php';
-    $categories = \App\Models\CategoriesModel\findAll($connexion);
+    $categories = \App\Models\CategoriesModel\findAllWithPostCount($connexion);
     foreach ($categories as $category):
     ?>
         <li>
-            <a href="#"><?php echo $category['name'] ?></a>
+            <a href="#"><?php echo $category['name'] ?> (<?php echo $category['post_count']; ?>)</a>
         </li>
     <?php endforeach; ?>
 </ul>
